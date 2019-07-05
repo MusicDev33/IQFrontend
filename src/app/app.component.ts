ed35ef681a43ca5f9555cd3b85005e1e27ee8042
MusicDev33 <smccowan33@gmail.com>
2019-07-05 17:33:13 -0600

    For now, this is what the home page will look like, that way the routes are hidden.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {Router, NavigationEnd} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-src';
+  currentRoute = ""
+  isHomePage = false
+
+  constructor(public activatedRoute: ActivatedRoute, public router: Router){
+
+  }
+
+  ngOnInit(){
+     this.router.events.subscribe(this.onUrlChange.bind(this))
+  }
+
+  onUrlChange(ev) {
+    if(ev instanceof NavigationEnd) {
+      let url = ev.url;
+      this.currentRoute = url
+      if (url === "/"){
+        this.isHomePage = true
+      }else{
+        this.isHomePage = false
+      }
+      console.log(this.isHomePage)
+      console.log(this.activatedRoute.outlet)
+    }
+  }
 }
