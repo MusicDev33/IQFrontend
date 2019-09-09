815d6a1eab4e647886af43b93b4a53998f5d9a7e
MusicDev33 <smccowan33@gmail.com>
2019-09-09 14:16:33 -0600

    Added support for MathJax

---
+++ src/assets/MathJax.js
@@ -0,0 +1,8 @@
+MathJax.Hub.Config({
+  config: ["MMLorHTML.js"],
+  jax: ["input/TeX","input/MathML","output/HTML-CSS","output/NativeMML"],
+  extensions: ["tex2jax.js","mml2jax.js","MathMenu.js","MathZoom.js"],
+  TeX: {
+    extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"]
+  }
+});
