65b685a4172232644d2d9f78e0276e28116a9cc2
MusicDev33 <smccowan33@gmail.com>
2019-09-20 07:50:35 -0600

    Dashboard SCSS instead of CSS

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -10,7 +10,7 @@ import { DebugService } from '../../services/debug.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
   user: any;
