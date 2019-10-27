147b7453d646a4af8043bf74147d6dd4fc287a91
MusicDev33 <smccowan33@gmail.com>
2019-10-27 17:00:34 -0600

    Greek keyboard with basic math for now

--- src/app/components/formatbar/formatbar.component.ts
+++ src/app/components/formatbar/formatbar.component.ts
@@ -13,10 +13,29 @@ export class FormatbarComponent implements OnInit {
 
   alphabetRows = [this.alphabetRow1, this.alphabetRow2, this.alphabetRow3];
 
+  // Greek Alphabet
+  gAlphabetRow1 = '\\epsilon,\\rho,\\tau,\\upsilon,\\theta,\\iota,\\omicron,\\pi';
+  gAlphabetRow2 = '\\alpha,\\sigma,\\delta,\\phi,\\gamma,\\eta,\\xi,\\kappa,\\lambda';
+  gAlphabetRow3 = '\\zeta,\\chi,\\psi,\\omega,\\beta,\\nu,\\mu';
+
+  gAlphabetRows = [this.gAlphabetRow1, this.gAlphabetRow2, this.gAlphabetRow3];
+
+  gUpperAlphabetRow1 = 'E,P,T,\\Upsilon,\\Theta,I,O,\\Pi';
+  gUpperAlphabetRow2 = 'A,\\Sigma,\\Delta,\\Phi,\\Gamma,H,Xi,K,\\Lambda';
+  gUpperAlphabetRow3 = 'Z,X,\\Psi,\\Omega,B,N,M';
+
+  gUpperAlphabetRows = [this.gUpperAlphabetRow1, this.gUpperAlphabetRow2, this.gUpperAlphabetRow3];
+
   shiftOn = false;
+
+  keyboardMode = 'Alphabet';
+  alphabetMode = 'English';
 
   @Output()
   letterClicked: EventEmitter<string> = new EventEmitter<string>();
+
+  @Output()
+  backspaceClicked: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
@@ -31,7 +50,37 @@ export class FormatbarComponent implements OnInit {
     }
   }
 
+  returnShiftedGAlphabetRow(row: number) {
+    if (this.shiftOn) {
+      return this.gUpperAlphabetRows[row].split(',');
+    } else {
+      return this.gAlphabetRows[row].split(',');
+    }
+  }
+
   addText(text: string) {
     this.letterClicked.emit('\\(' + text + '\\)');
+  }
+
+  backspace() {
+    this.backspaceClicked.emit(true);
+  }
+
+  shiftKeyPressed() {
+    this.shiftOn = !this.shiftOn;
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+  }
+
+  keyboardModeChange(mode: string) {
+    this.keyboardMode = mode;
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+  }
+
+  changeAlphabetMode(text: string) {
+    this.alphabetMode = text;
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
   }
 }
