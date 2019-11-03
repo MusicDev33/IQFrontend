9e6645254144b558e2493fa5a6e8779e30f5e76d
MusicDev33 <smccowan33@gmail.com>
2019-11-03 10:13:25 -0700

    Polyfill prep

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,22 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+/*
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/weak-map';
+import 'core-js/es6/set';
+import 'core-js/es6/array';
+import 'core-js/es6/reflect';
+import 'web-animations-js';
+*/
