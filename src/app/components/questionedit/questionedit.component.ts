53e53cc3b120776f1ac793534f4af60c846ecd8b
MusicDev33 <smccowan33@gmail.com>
2019-11-24 18:28:41 -0700

    More UI for editing questions

--- src/app/components/questionedit/questionedit.component.ts
+++ src/app/components/questionedit/questionedit.component.ts
@@ -7,15 +7,20 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
   styleUrls: ['./questionedit.component.scss']
 })
 export class QuestionEditComponent implements OnInit {
+  question: any;
+  sourceName = '';
+
+  testTags = ['animals', 'mario', 'tag9 and long', 'tekken', 'toofast', 'chapter 9', 'chapter 347', 'the bible verse 7'];
 
   constructor(
     public dialogRef: MatDialogRef<QuestionEditComponent>,
     @Inject(MAT_DIALOG_DATA) data
   ) {
-    console.log(data);
+    this.question = data.question;
   }
 
   ngOnInit() {
+    this.sourceName = this.question.homeworkSource;
   }
 
 }
