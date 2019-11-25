f80ef8c5ab52d7395f91181f75590c3b900525fc
MusicDev33 <smccowan33@gmail.com>
2019-11-25 12:42:42 -0700

    Increment for Edit Question UI

--- src/app/components/questionedit/questionedit.component.ts
+++ src/app/components/questionedit/questionedit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
+import { DebugService } from '@services/utility/debug.service';
+
 @Component({
   selector: 'app-questionedit',
   templateUrl: './questionedit.component.html',
@@ -17,10 +19,12 @@ export class QuestionEditComponent implements OnInit {
   addTagText = '';
 
   constructor(
+    public debug: DebugService,
     public dialogRef: MatDialogRef<QuestionEditComponent>,
     @Inject(MAT_DIALOG_DATA) data
   ) {
     this.question = data.question;
+    this.debug.log(data);
   }
 
   ngOnInit() {
@@ -32,4 +36,17 @@ export class QuestionEditComponent implements OnInit {
     this.addTagText = 'Add Tag';
   }
 
+  addTag(tagName: string) {
+    if (tagName !== '') {
+      this.question.tags.push(tagName);
+      this.newTagName = '';
+    }
+  }
+
+  deleteTag(tagName: string) {
+    const index = this.question.tags.indexOf(tagName);
+    if (index !== -1) {
+      this.question.tags.splice(index, 1);
+    }
+  }
 }
