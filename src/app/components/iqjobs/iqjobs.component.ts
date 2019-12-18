9d3f0cc61599490017691799ce33f61c0b5f0590
MusicDev33 <smccowan33@gmail.com>
2019-12-17 17:02:52 -0700

    Dynamicize the job page

--- src/app/components/iqjobs/iqjobs.component.ts
+++ src/app/components/iqjobs/iqjobs.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IQJobsComponent implements OnInit {
 
+  jobsList: Array<string>;
+  jobIndex = 0;
+  fullNameField = '';
+
   constructor() { }
 
   ngOnInit() {
+    this.jobsList = ['Content Management Agent'];
   }
 
+  increaseJobIndex() {
+    if (this.jobIndex >= this.jobsList.length - 1) {
+      this.jobIndex = this.jobsList.length - 1;
+    } else {
+      this.jobIndex += 1;
+    }
+  }
+
+  decreaseJobIndex() {
+    console.log("hi")
+    if (this.jobIndex <= 0) {
+      this.jobIndex = 0;
+    } else {
+      this.jobIndex -= 1;
+    }
+  }
+
+  clickedOnApply() {
+    
+  }
 }
